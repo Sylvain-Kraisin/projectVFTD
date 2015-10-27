78948c738687f1b78ed3f5b2063d717e606367bc
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2015-10-27 17:52:46 +0100

    model Post created and migrated (title:string content:text author:string)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151015205938) do
+ActiveRecord::Schema.define(version: 20151027164829) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.string   "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
