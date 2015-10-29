c18ddf2418743d5d3b527158644f54d6728cf04a
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2015-10-29 20:47:08 +0100

    blog section : added Edit(def update def edit), Delete(def destroy), Back links to posts/*

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all.order("created_at DESC")
   end
@@ -24,13 +26,24 @@ class PostsController < ApplicationController
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
     def post_params
       params.require(:post).permit(:title, :content, :author)
+    end
+
+    def find_post
+      @post = Post.find(params[:id])
     end
 end
