1dc98c0117b7547cc7fdb3d468a51b37a49bc757
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2015-10-30 15:48:34 +0100

    Username field fully working

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username,
+    :presence => true,
+    :uniqueness => {
+    :case_sensitive => false
+    }
 end
