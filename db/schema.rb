fd0567ac9d3ff36219dfd437a7bcc26d04f1f227
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2015-10-30 01:52:00 +0100

    added username field to devise BUT Sign Up works even if Username is blank. Username works in User DB anyway. App Error Page if Username already exist when create new User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027164829) do
+ActiveRecord::Schema.define(version: 20151029230600) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -34,9 +34,11 @@ ActiveRecord::Schema.define(version: 20151027164829) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
