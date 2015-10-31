be5ebba89f3691287fabb45dd172c84fe2c2ee3e
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2015-10-31 15:23:25 +0100

    fixed posts/1/edit & posts/new navbar access only to admin/ created def admin? in application_controller and before_filter in post_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,14 @@ class ApplicationController < ActionController::Base
 
   protected
 
+ def admin?
+   if signed_in?
+     redirect_to root_path unless current_user.role == "admin"
+   else
+     redirect_to root_path
+   end
+ end
+
  def configure_permitted_parameters
   devise_parameter_sanitizer.for(:sign_up) << :username
   devise_parameter_sanitizer.for(:sign_up) << :role
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
-
+  before_filter :admin?, only: [:new, :create, :edit, :update, :destroy]
   def index
     @posts = Post.all.order("created_at DESC")
 
