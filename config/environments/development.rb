4e4cee698d26cda807abd5b2e5570401709e0463
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2015-11-03 21:39:23 +0100

    add mandrill to config/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,17 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'viensfairetesdevoirs.dev:3000' }
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.mandrillapp.com',
+    port:                 587,
+    domain:               'viensfairetesdevoirs.dev',
+    user_name:            'sylvain.kraisin@gmail.com',
+    password:             ENV["MANDRILL_API_KEY"],
+    authentication:       'login',
+    enable_starttls_auto: true  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
