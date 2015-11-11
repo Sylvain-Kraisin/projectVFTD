fcb95bc46c1b08c5eb85d6bb13ee842f26614118
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2015-11-11 23:30:07 +0100

    some bootstrap front for images on posts/index

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :category
 
-  has_attached_file :post_img, styles: { post_index: "250x350>", post_show: "325x475>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :post_img, styles: { post_index: "180x216>", post_show: "325x475>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :post_img, content_type: /\Aimage\/.*\Z/
 end
