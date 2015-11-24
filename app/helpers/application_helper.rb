f01011b3fb5aa7d092462a81743c863c685a4f87
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2015-11-24 17:43:03 +0100

    add markdownadmin and markdown (non html) to application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,12 @@
 module ApplicationHelper
 
-  def markdown
+  def markdownadmin
     renderer = Redcarpet::Render::HTML.new(render_options = {hard_wrap: true, with_toc_data: true, safe_links_only: true, highlight: true})
+    Redcarpet::Markdown.new(renderer, extensions = {autolink: true, strikethrough: true, quote: true})
+  end
+
+  def markdown
+    renderer = Redcarpet::Render::HTML.new(render_options = {filter_html: true, hard_wrap: true, with_toc_data: true, safe_links_only: true, highlight: true})
     Redcarpet::Markdown.new(renderer, extensions = {autolink: true, strikethrough: true, quote: true})
   end
 
