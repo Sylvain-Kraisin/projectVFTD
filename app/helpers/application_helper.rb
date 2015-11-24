cc8cd22d8cae7e7f561570ce18f5e6a025f166a9
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2015-11-24 03:19:53 +0100

    minors changes posts/show front

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 
   def markdown
-    renderer = Redcarpet::Render::HTML.new(render_options = {hard_wrap: true})
-    Redcarpet::Markdown.new(renderer, extensions = {autolink: true})
+    renderer = Redcarpet::Render::HTML.new(render_options = {hard_wrap: true, highlight: true, quote: true})
+    Redcarpet::Markdown.new(renderer, extensions = {autolink: true, safe_links_only: true})
   end
 
 end
