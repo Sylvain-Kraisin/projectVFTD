5eb09eb6c5402923a59302c787464f90a4909fa6
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2015-11-24 05:07:16 +0100

    some posts/show front stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 
   def markdown
-    renderer = Redcarpet::Render::HTML.new(render_options = {hard_wrap: true, highlight: true, quote: true})
-    Redcarpet::Markdown.new(renderer, extensions = {autolink: true, safe_links_only: true})
+    renderer = Redcarpet::Render::HTML.new(render_options = {hard_wrap: true, with_toc_data: true, safe_links_only: true, highlight: true})
+    Redcarpet::Markdown.new(renderer, extensions = {autolink: true, strikethrough: true, quote: true})
   end
 
 end
