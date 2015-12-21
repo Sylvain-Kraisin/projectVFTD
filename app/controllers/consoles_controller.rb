cafebad0602eccef2471d86bce13c265103fdf5c
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2015-12-21 17:32:28 +0100

    change view/kaminari/paginator to show just the 4th, no more prev next first last links

--- app/controllers/consoles_controller.rb
+++ app/controllers/consoles_controller.rb
@@ -6,7 +6,7 @@ class ConsolesController < ApplicationController
   def show
     @console = Console.find(params[:id])
     @videos = Video.all
-    @videok = @videos.where(console_id: @console.id).page(params[:page]).per(5).order("created_at ASC")
+    @videok = @videos.where(console_id: @console.id).page(params[:page]).per(1).order("created_at ASC")
   end
 
 end
