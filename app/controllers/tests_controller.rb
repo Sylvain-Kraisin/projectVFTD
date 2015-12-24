1936f62c23ba9adbc99b3905ef84d3fc7f5d05af
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2015-12-24 04:57:23 +0100

    almost finished css tests/show and minors css changes

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,6 +1,27 @@
 class TestsController < ApplicationController
-
+before_action :find_test
   def show
   end
+
+  def edit
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to root_path
+    else
+      render 'show'
+    end
+  end
+
+  private
+    def test_params
+      params.require(:test).permit(:video_id, :video_title, :question_1, :reponse_1, :answer_1, :question_2, :reponse_2, :answer_2, :question_3, :reponse_3, :answer_3, :question_4, :reponse_4, :answer_4, :note, :done)
+    end
+
+    def find_test
+      @test = Test.find(params[:id])
+    end
+
 
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,9 @@
 class Test < ActiveRecord::Base
   belongs_to :video
   belongs_to :user
+
+  validates :reponse_1, presence: {}
+  validates :reponse_2, presence: {}
+  validates :reponse_3, presence: {}
+  validates :reponse_4, presence: {}
 end
