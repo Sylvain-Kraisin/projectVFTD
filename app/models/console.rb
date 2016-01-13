b3c8e735e6ea1cd04285c71bc7477a2e813b2d9b
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2016-01-14 00:19:57 +0100

    front stuff @consoles/show

--- app/models/console.rb
+++ app/models/console.rb
@@ -1,6 +1,6 @@
 class Console < ActiveRecord::Base
   has_many :videos
 
-  has_attached_file :console_img, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :console_img, styles: { medium: "256x256>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :console_img, content_type: /\Aimage\/.*\Z/
 end
