7417ea947c1922cb73962425b7cc2024b082bd30
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2016-01-17 23:34:29 +0100

    2Mo for avatar ok, front@users/edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ":style/avatar.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   validates_attachment_size :avatar, :in => 0.megabytes..2.megabytes
-                                            
+
 
   validates :username,
     :presence => true,
