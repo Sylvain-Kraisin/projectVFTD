94eb284a0edd9520a94ec3855f185be2e872f3b2
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2016-01-19 16:40:47 +0100

    updated mailing prod with true domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'http://vps215774.ovh.net' }
+  config.action_mailer.default_url_options = { :host => 'http://viensfairetesdevoirs.com' }
   config.action_mailer.smtp_settings = {
     address:              'smtp.mandrillapp.com',
     port:                 587,
