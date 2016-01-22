ea2e5cda60d8b65d6f0d937ff14f36c41b2d1fa7
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2016-01-22 01:37:43 +0100

    trying add confirmable to devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160117141414) do
+ActiveRecord::Schema.define(version: 20160121185947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
