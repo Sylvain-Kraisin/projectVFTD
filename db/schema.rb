b583ceb00950c3c554e0bc42821751242506af3f
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2016-01-22 02:19:17 +0100

    add confirmable to devise for emails. Working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160117141414) do
+ActiveRecord::Schema.define(version: 20160122010830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,8 +111,12 @@ ActiveRecord::Schema.define(version: 20160117141414) do
     t.datetime "avatar_updated_at"
     t.integer  "presence"
     t.float    "average"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["role"], name: "index_users_on_role", unique: true, using: :btree
