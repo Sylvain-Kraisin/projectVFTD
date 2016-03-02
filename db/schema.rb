75956c824be5cbda77aeae513e421eb309abb1cd
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2016-03-02 20:38:25 +0100

    add score to User. Update all scores

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301184734) do
+ActiveRecord::Schema.define(version: 20160302192649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(version: 20160301184734) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.float    "score"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
