ffcc0eff4d6d68049d7b1414cc8e3e8a5c4d3f06
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2016-03-13 23:37:09 +0100

    score test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -92,7 +92,8 @@ layout :resolve_layout
 
     @goodusers_average.each do |youser|
       @dst_notnil = Reponse.where("total is NOT NULL").where(user_username: youser.username)
-      @score = youser.average.round(2) * @dst_notnil.count
+      @score = youser.average * @dst_notnil.count
+      youser.update average:youser.reponses.average(:total).round(2)
       youser.update score:@score.round(3)
     end
   end
