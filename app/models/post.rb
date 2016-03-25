9efa8d91ef37db02730a099be165f250f1179090
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2016-03-25 15:09:14 +0100

    add dependent destroy to comments/post model and has_one to test/video  model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :category
   has_many :visits
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   has_attached_file :post_img, styles: { post_index: "180x216>", post_show: "90x108>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :post_img, content_type: /\Aimage\/.*\Z/
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,6 @@
 class Video < ActiveRecord::Base
   belongs_to :console
-  has_many :tests
+  has_one :test, dependent: :destroy
 
   has_attached_file :video_img, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :video_img, content_type: /\Aimage\/.*\Z/
