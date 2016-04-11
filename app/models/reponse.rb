afd05f6fce342b81bd84e5c8f32787ead8453265
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2016-04-11 16:33:59 +0200

    fix notes validates

--- app/models/reponse.rb
+++ app/models/reponse.rb
@@ -3,10 +3,10 @@ belongs_to :test
 belongs_to :user
 
   #ne valide que les notes entre 0 et 4
-  validates :note_1, :inclusion => {:in => 0..4, :only_float => true }
-  validates :note_2, :inclusion => {:in => 0..4, :only_float => true }
-  validates :note_3, :inclusion => {:in => 0..4, :only_float => true }
-  validates :note_4, :inclusion => {:in => 0..4, :only_float => true }
+  validates :note_1, :inclusion => {:in => 0..4, :only_float => true, :allow_blank => true}
+  validates :note_2, :inclusion => {:in => 0..4, :only_float => true, :allow_blank => true}
+  validates :note_3, :inclusion => {:in => 0..4, :only_float => true, :allow_blank => true}
+  validates :note_4, :inclusion => {:in => 0..4, :only_float => true, :allow_blank => true}
 
   #un meme user ne peut creer 2 fois une reponse
   validates :user_username, uniqueness: { scope: [:user_username, :test_id], message: 'Tu as déjà passé ce DST !'}
