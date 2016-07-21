26034edae2d29976a63bef1e85941e34ea80a0c9
Sylvain Kraisin <sylvain.kraisin@gmail.com>
2016-07-21 22:09:18 +0200

    fck fb scraping

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
   before_action :create_visit, only: [:show]
-  before_action :render_meta, only: [:show]
   before_filter :bibliothequaire?, only: [:new, :create, :edit, :update]
   before_filter :admin?, only: :destroy
 
@@ -53,10 +52,6 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     redirect_to posts_path
-  end
-
-  def render_meta
-    render partial: "posts_meta"
   end
 
   private
