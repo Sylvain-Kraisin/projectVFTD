39def45a1921d79440f8684f1a04c1b884ffa2b6
Sylvain-Kraisin <sylvain.kraisin@gmail.com>
2017-04-10 15:10:28 +0200

    gift month update

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
   has_many :visits
-  has_many :reponses, dependent: :destroy
+  has_many :reponses
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
