932e767be1cb9060f5482f98e3010206421df711
Sylvain-Kraisin <sylvain.kraisin@gmail.com>
2017-07-31 17:18:03 +0200

    whenever/rbenv fix

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,6 +19,28 @@
 
 # Learn more: http://github.com/javan/whenever
 
+# Schedule script for using Whenever toghether with rbenv
+#
+#   Whenever: https://github.com/javan/whenever
+#   rbenv:    https://github.com/sstephenson/rbenv
+
+if Rails.env.production?
+  set :path, "/home/koan_dev/www/projectVFTD"
+  set :env_path,    '"/home/koan_dev/.rbenv/shims":"/home/koan_dev/.rbenv/bin"'
+
+  # doesn't need modifications
+  # job_type :command, ":task :output"
+
+  job_type :rake,   %q{ cd :path && PATH=:env_path:"$PATH" RAILS_ENV=:environment bundle exec rake :task --silent :output }
+  job_type :runner, %q{ cd :path && PATH=:env_path:"$PATH" script/rails runner -e :environment ':task' :output }
+  job_type :script, %q{ cd :path && PATH=:env_path:"$PATH" RAILS_ENV=:environment bundle exec script/:task :output }
+end
+
+# tasks
 every :wednesday, :at => '1pm' do
   runner "Post.validated.order('created_at ASC').first.try(:publish!)"
+end
+
+every 1.minute do
+  runner "User.last.update female:true"
 end
